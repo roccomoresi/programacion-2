c270821715b0673f3ab9129aec39117179ed6a6a
Rocco Moresi <maxirocco3@gmail.com>
2024-08-08 15:30:19 -0300

    ejercicio 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        int c = 8;
+        int mayor = a;
+
+        if(a >= b && a >= c){
+            mayor = a;
+        }
+
+        if(b >= c && b >= a){
+            mayor = b;
+        }
+
+        if(c >= b && c >= a){
+            mayor = c;
+        }
+        System.out.println("El mayor es ---> " + mayor);
+    }
+}
